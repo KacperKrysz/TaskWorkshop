969a8f3c18fab48c99da24a19c802c78bdb2b153
KacperKrysz <kacperkrysz@gmail.com>
2023-04-23 12:44:03 +0200

    Menu

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,7 +1,30 @@
 package pl.coderslab;
 
+import pl.coderslab.ConsoleColors;
+
 public class TaskManager {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        menu();
+
     }
+
+
+    public static void menu() {
+
+        String[] lines = new String[5];
+
+        lines[0] = ConsoleColors.BLUE + "Please select an option";
+        lines[1] = ConsoleColors.RESET + "add";
+        lines[2] = "remove";
+        lines[3] = "list";
+        lines[4] = "exit";
+
+        for (String line : lines) {
+
+            System.out.println(line);
+
+        }
+    }
+
 }
